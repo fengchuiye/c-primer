9e46b6162e264cb8e99e7987b766dd04267eebf8
fengchuiye <fengchuiye@yeah.net>
2018-04-10 08:36:45 +0800

    提交此前已经不记得的代码

---
+++ 3.27testArray/testArray.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<iostream>
+#include<string>
+using namespace std;
+
+string sa[10];
+int ia[10];
+
+int main(){
+    string sa2[10];
+    int    ia2[10];
+
+    cout << sa[0] << " " << ia[0] << " " << sa2[0] << " " << ia2[0] << endl;
+
+    unsigned int size = 10;
+    int aa[size];
+
+    return 0;
+}
